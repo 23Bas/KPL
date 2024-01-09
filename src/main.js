7743c16eabc9651069a7a5ae41ca2ffe14b534a2
Baskara <agung.ngurah145@gmail.com>
2024-01-09 22:07:00 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import "./style.css";
+import { getIPLocation } from "./getIPLocation";
+import { getAQIData } from "./getAQIData";
+import { displayData } from "./displayData";
+import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+
+
+const location = await getIPLocation();
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: location || [72.83333, 18.96667], // starting position [lng, lat]
+  zoom: 12.5, // starting zoom
+});
+
+map.on("render", function () {
+  map.resize();
+});
+
+const geocoder = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  placeholder: "Search for places",
+  mapboxgl: mapboxgl,
+});
+
+map.addControl(geocoder);
+
+geocoder.on("result", async (results) => {
+  // console.log(results);
+  const val = await getAQIData(results.result.center);
+  // console.log(val);
+  displayData(val);
+});
+
+const AQIData = await getAQIData(location);
+// console.log(AQIData);
+displayData(AQIData);
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require("tailwindcss/colors");
+const Nth = require("tailwindcss-nth-child");
+const plugin = new Nth("2");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode:'class',
+  theme: {
+    fontFamily: {
+      display: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        white: "#faf9f9",
+        background: "#1d1d23",
+        cyan: colors.cyan,
+        green: colors.emerald,
+        orange: colors.orange,
+        purple: colors.purple,
+        rose: colors.rose,
+      },
+    },
+  },
+  plugins: [plugin.nthChild()],
+};
